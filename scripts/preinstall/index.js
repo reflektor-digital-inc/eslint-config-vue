ac9921c2dd03a3e7835aacaf84dc3fdd923f4615
wlto <wltowxyz@gmail.com>
2021-12-17 12:16:50 -0500

    chore: update yarn version

--- scripts/preinstall/index.js
+++ scripts/preinstall/index.js
@@ -1,6 +1,7 @@
 const pleaseUseYarn = () => {
   const isUsingYarn = process.env.npm_config_user_agent.includes('yarn');
   if (!isUsingYarn) {
+    // eslint-disable-next-line no-console
     console.info('\n🧶 Please use yarn for this project.\n');
     process.exit(1);
   }
